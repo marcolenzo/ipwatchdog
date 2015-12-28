b1a8821dd938d67cc14f47f66c56008750030032
Marco Lenzo <lenzo.marco@gmail.com>
2015-12-28 11:51:07 +0100

    Add callback mechanism

--- ipwatchdog.go
+++ ipwatchdog.go
@@ -9,17 +9,20 @@ import (
 	"net/smtp"
 )
 
-var lastIp, from, to, server_host, server_port, user, passwd, schedule, checkip_url string
+var lastIp, from, to, server_host, server_port, user, passwd, schedule, checkip_url, callback_url, callback_ip_param, callback_auth_header string
 
 func main() {
 	flag.StringVar(&from, "from", "", "Sender email address, e.g. user@domain.com")
-	flag.StringVar(&to, "to", "", "Recipient email address, e.g. recipient@domain.com")
+	flag.StringVar(&to, "to", "", "Recipient email address, e.g. recipient@domain.com.")
 	flag.StringVar(&server_host, "server_host", "smtp.gmail.com", "Mail server hostname.")
 	flag.StringVar(&server_port, "server_port", "587", "Mail server port.")
 	flag.StringVar(&user, "user", "", "Username @ Mail server. If not specified the sender email address is used.")
 	flag.StringVar(&passwd, "passwd", "", "Password @ Mail server.")
 	flag.StringVar(&schedule, "schedule", "@every 30min", "Schedule defining the time interval between each IP check.")
 	flag.StringVar(&checkip_url, "checkip_url", "http://checkip.amazonaws.com", "Check IP API URL.")
+	flag.StringVar(&callback_url, "callback_url", "", "URL to hit when IP changes. If not set, callback won't be performed.")
+	flag.StringVar(&callback_ip_param, "callback_ip_param", "", "Query parameter to be used to communicate new IP. If left empty IP won't be set.")
+	flag.StringVar(&callback_auth_header, "callback_auth_header", "", "Authorization header to be used in callback.")
 	flag.Parse()
 	if from == "" || to == "" {
 		panic("-from and -to parameters are required.")
@@ -48,6 +51,9 @@ func checkIp() {
 		message := "IP has changed! Previous was: " + lastIp
 		fmt.Println(message)
 		sendMail([]byte("IP has changed! New IP is " + currentIp + " while previous IP was " + lastIp))
+		if callback_url != "" {
+			callback(currentIp)
+		}
 	}
 	lastIp = currentIp
 }
@@ -59,4 +65,29 @@ func sendMail(message []byte) {
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+func callback(ip string) {
+	url := callback_url
+	if callback_ip_param != "" {
+		url = callback_url + callback_ip_param + ip
+	}
+	fmt.Println("Invoking callback url: " + url)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if callback_auth_header != "" {
+		fmt.Println("Setting Authorization Header to: " + callback_auth_header)
+		req.Header.Add("Authorization", callback_auth_header)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
 }
